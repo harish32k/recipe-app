cef16c8cd42ef62590f487513f88c0d88b26ce14
Harish Akula <harish.akula32k@gmail.com>
2023-12-14 00:31:11 -0500

    added favouriting

--- src/RecipeApp/App/CategoryPosts.js
+++ src/RecipeApp/App/CategoryPosts.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import MealPost from "./MealPost";
 import * as recipeClient from "../Clients/recipeClient.js";
+import * as favClient from "../Clients/favouritesClient.js";
 import { useParams } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function CategoryPosts() {
   const { category } = useParams();
   const [posts, setPosts] = useState([]);
-
   const getPosts = async () => {
     try {
       const data = await recipeClient.fetchCategoryByName(category);
@@ -21,10 +22,47 @@ function CategoryPosts() {
     getPosts();
   }, []);
 
+  const currUser = useSelector((state) => state.userReducer.user);
+
+  const [favouritedStatus, setFavouritedStatus] = useState(false);
+  const fetchFavouriteStatus = async () => {
+    try {
+      const response = await favClient.fetchFavouritedStatus(currUser._id, category);
+      console.log("STATUS", response.favourited)
+      setFavouritedStatus(response.favourited);
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
+  useEffect(() => {
+    fetchFavouriteStatus();
+  }, [category]);
+
+  
+  const handleFavourite = async () => {
+    try {
+      const response = await favClient.addFavouriteCategory(currUser._id, category);
+      await fetchFavouriteStatus();
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
+
+  const handleUnfavourite = async () => {
+    try {
+      const response = await favClient.removeFavouriteCategory(currUser._id, category);
+      await fetchFavouriteStatus();
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
+
   return (
     <Container>
       <h1>{category}</h1>
-      {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
+      {favouritedStatus ?
+        <button className="btn btn-success" onClick={() => handleUnfavourite()}>Favourited</button> :
+        <button className="btn btn-outline-success"onClick={() => handleFavourite()}>Favourite</button>}
       <Row>
         {posts.map((post) => (
           <MealPost key={post._id} post={post} />
--- src/RecipeApp/Clients/favouritesClient.js
+++ src/RecipeApp/Clients/favouritesClient.js
@@ -3,12 +3,39 @@ import axios from "axios";
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const RECIPE_API = `${BASE_API}/api/favcategory`;
 
+const request = axios.create({
+    baseURL: RECIPE_API,
+    withCredentials: true,
+  });
+  
 export const fetchUserFavourites = async (userId) => {
-    const response = await axios.get(`${RECIPE_API}/list/users/${userId}`);
+    const response = await request.get(`${RECIPE_API}/list/users/${userId}`);
     return response.data;
 };
 
 export const fetchFavouritedStatus = async (userId, category) => {
-    const response = await axios.get(`${RECIPE_API}/status/users/${userId}/category/${category}`);
+    const response = await request.get(`${RECIPE_API}/status/users/${userId}/category/${category}`);
     return response.data;
+};
+
+export const addFavouriteCategory = async (userId, category) => {
+    try {
+        const response = await request.post(`${RECIPE_API}/user/${userId}/category/${category}`);
+        return response.data;
+    } catch (error) {
+        // Handle error here
+        console.error("Error adding favourite category:", error);
+        throw error; // Throw error to handle it in the calling function
+    }
+};
+
+export const removeFavouriteCategory = async (userId, category) => {
+    try {
+        const response = await request.delete(`${RECIPE_API}/user/${userId}/category/${category}`);
+        return response.data;
+    } catch (error) {
+        // Handle error here
+        console.error("Error removing favourite category:", error);
+        throw error; // Throw error to handle it in the calling function
+    }
 };
