0be04802736a54b5c8ec588dfbf90486cfe41378
Harish Akula <harish.akula32k@gmail.com>
2023-12-13 19:25:09 -0500

    updated comments client

--- src/RecipeApp/Clients/commentClient.js
+++ src/RecipeApp/Clients/commentClient.js
@@ -3,13 +3,18 @@ import axios from "axios";
 export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const COMMENT_API = `${BASE_API}/api/comment`;
 
+const request = axios.create({
+  baseURL: COMMENT_API,
+  withCredentials: true,
+});
+
 export const fetchCommentsOfPost = async (postId) => {
-  const response = await axios.get(`${COMMENT_API}/recipe/${postId}`);
+  const response = await request.get(`${COMMENT_API}/recipe/${postId}`);
   return response.data;
 };
 
 export const addComment = async (recipeId, userId, strComment) => {
-  const response = await axios.post(`${COMMENT_API}/recipe`, {
+  const response = await request.post(`${COMMENT_API}/recipe`, {
     recipeId,
     userId,
     strComment,
@@ -18,11 +23,11 @@ export const addComment = async (recipeId, userId, strComment) => {
 };
 
 export const removeComment = async (_id) => {
-  const response = await axios.delete(`${COMMENT_API}/recipe/commentId/${_id}`);
+  const response = await request.delete(`${COMMENT_API}/recipe/commentId/${_id}`);
   return response.data;
 };
 
 export const fetchCommentsByUser = async (_id) => {
-  const response = await axios.get(`${COMMENT_API}/user/${_id}`);
+  const response = await request.get(`${COMMENT_API}/user/${_id}`);
   return response.data;
 };
